3e62a7e9191a0e4ccea1c2df86be4dcc6ccce5f7
Ryosuke1025 <ryosuke131025@icloud.com>
2023-06-16 14:10:47 +0900

    [Add] 天気の画像がリロードされるようにした

--- ios-training-Ryosuke/ViewController.swift
+++ ios-training-Ryosuke/ViewController.swift
@@ -6,11 +6,26 @@
 //
 
 import UIKit
-
+import YumemiWeather
 class ViewController: UIViewController {
-
+    
+    var weather: String = ""
+    @IBOutlet weak var weatherImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func reload(_ sender: Any) {
+        weather = YumemiWeather.fetchWeatherCondition()
+        weatherImage.image = UIImage(named: weather)
+        if(weather == "sunny") {
+            weatherImage.tintColor = .red
+        } else if(weather == "cloudy") {
+            weatherImage.tintColor = .gray
+        } else {
+            weatherImage.tintColor = .blue
+        }
     }
 }
