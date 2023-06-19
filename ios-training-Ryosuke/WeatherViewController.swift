07235b2917c1ae3bcc3de49756a38fd914d15865
Ryosuke1025 <ryosuke131025@icloud.com>
2023-06-19 11:17:09 +0900

    [Add] closeボタンを押したら、新しい画面に遷移するようにした

--- ios-training-Ryosuke/WeatherViewController.swift
+++ ios-training-Ryosuke/WeatherViewController.swift
@@ -16,6 +16,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+
+    @IBAction func close(_ sender: Any) {
+        let blackVC = BlackViewController()
+        blackVC.modalPresentationStyle = .fullScreen
+        self.present(blackVC, animated: true, completion: nil)
+    }
+    
     @IBAction private func reload(_ sender: Any) {
         var weather: String = ""
         weather = YumemiWeather.fetchWeatherCondition()
