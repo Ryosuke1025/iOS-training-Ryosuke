ad99f0a1beb40bed5fb12c2f7a2dceb50da695ec
Ryosuke1025 <ryosuke131025@icloud.com>
2023-06-26 13:16:25 +0900

    [Fix] encodeとdecode用の関数を作り、ネストを浅くした

--- ios-training-Ryosuke/Model/WeatherModel.swift
+++ ios-training-Ryosuke/Model/WeatherModel.swift
@@ -16,30 +16,30 @@ final class WeatherModel {
     weak var delegate: WeatherModelDelegate?
     
     func fetchWeatherCondition() {
-        let request = RequestModel(area: "tokyo", date: "2020-04-01T12:00:00+09:00")
         do {
-            let requestData = try JSONEncoder().encode(request)
-            guard let requestString = String(data: requestData, encoding: .utf8) else {
-                assertionFailure("リクエストデータの文字列変換に失敗しました")
-                return
-            }
-            do {
-                let responseString = try YumemiWeather.fetchWeather(requestString)
-                guard let responseData = responseString.data(using: .utf8) else {
-                    assertionFailure("レスポンス文字列のデータ変換に失敗しました")
-                    return
-                }
-                do {
-                    let response = try JSONDecoder().decode(ResponseModel.self, from: responseData)
-                    delegate?.didFetchWeatherCondition(response: response)
-                } catch {
-                    print("レスポンスデータのデコードに失敗しました")
-                }
-            } catch {
-                delegate?.failedFetchWeatherCondition()
-            }
+            let request = RequestModel(area: "tokyo", date: "2020-04-01T12:00:00+09:00")
+            let requestString = try encode(request: request)
+            let responseString = try YumemiWeather.fetchWeather(requestString)
+            let response = try decode(responseString: responseString)
+            delegate?.didFetchWeatherCondition(response: response)
         } catch {
-            print("レスポンスデータのエンコードに失敗しました")
+            delegate?.failedFetchWeatherCondition()
         }
+    }
+    
+    private func encode(request: RequestModel) throws -> String {
+        let requestData = try JSONEncoder().encode(request)
+        guard let requestString = String(data: requestData, encoding: .utf8) else {
+            throw EncodingError.invalidValue(request, EncodingError.Context(codingPath: [], debugDescription: "Request data could not be converted to a string."))
+        }
+        return requestString
+    }
+    
+    private func decode(responseString: String) throws -> ResponseModel {
+        guard let responseData = responseString.data(using: .utf8) else {
+            throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: [], debugDescription: "Response string could not be converted to Data."))
+        }
+        let response = try JSONDecoder().decode(ResponseModel.self, from: responseData)
+        return response
     }
 }
