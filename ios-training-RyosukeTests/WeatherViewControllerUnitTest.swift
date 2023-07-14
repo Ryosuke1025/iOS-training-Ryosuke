b3cbcc129d6912630518d8caccdfd7afc2fa13c0
Ryosuke1025 <ryosuke131025@icloud.com>
2023-07-14 12:45:39 +0900

    [Fix] テストコードの書き換え

--- ios-training-RyosukeTests/WeatherViewControllerUnitTest.swift
+++ ios-training-RyosukeTests/WeatherViewControllerUnitTest.swift
@@ -8,6 +8,7 @@
 import XCTest
 @testable import ios_training_Ryosuke
 
+@MainActor
 final class WeatherViewControllerUnitTest: XCTestCase {
     
     var weatherViewController: WeatherViewController!
@@ -38,13 +39,11 @@ final class WeatherViewControllerUnitTest: XCTestCase {
 
             await weatherViewController.reload(UIButton())
             Task {
-                await XCTAssertEqual(weatherViewController.weatherImage.image, UIImage(named: weather.condition.rawValue)?.withRenderingMode(.alwaysTemplate))
-                await XCTAssertEqual(weatherViewController.maxTemperatureLabel.text, String(weather.maxTemp))
-                await XCTAssertEqual(weatherViewController.minTemperatureLabel.text, String(weather.minTemp))
+                XCTAssertEqual(weatherViewController.weatherImage.image, UIImage(named: weather.condition.rawValue)?.withRenderingMode(.alwaysTemplate))
+                XCTAssertEqual(weatherViewController.maxTemperatureLabel.text, String(weather.maxTemp))
+                XCTAssertEqual(weatherViewController.minTemperatureLabel.text, String(weather.minTemp))
             }
-            
         }
-        
     }
 }
 
